ddf96c6be21c9bc48bc3cc42d5e19e9b29617cb5
Zyzzzaaa <842511142@qq.com>
2023-07-13 17:13:12 +0800

    🐣feat:drawer样式基本完成

--- src/drawer/index.tsx
+++ src/drawer/index.tsx
@@ -1,62 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react'
 import classNames from "classnames";
 import './style/index.less'
 import { IconClose } from 'cherry-ui/icons';
 
-interface DrawerDefaultProps{
+interface DrawerDefaultProps {
     // 是否有关闭按钮
-    close?:boolean;
+    close?: boolean;
     // 关闭按钮回调函数
-    onClolse:(event?:React.MouseEvent<HTMLElement>|React.KeyboardEvent<HTMLElement>)=>void;
+    onClolse: (event?: React.MouseEvent<HTMLElement> | React.KeyboardEvent<HTMLElement>) => void;
     // 是否放置在当前dom中
-    inline?:boolean;
+    inline?: boolean;
     // 弹出框是否弹出
-    open?:boolean;
+    open?: boolean;
 
     // root根节点
-    rootClassname?:string;
-    rootStyle?:React.CSSProperties;
-    zIndex?:number;
+    rootClassname?: string;
+    rootStyle?: React.CSSProperties;
+    zIndex?: number;
 
-    title?:string;
+    title?: string;
 
-    children?:React.ReactNode
+    children?: React.ReactNode
 }
 
 type NativeDrawer = DrawerDefaultProps & React.HTMLAttributes<HTMLElement>
 export type DrawerProps = Partial<NativeDrawer>
 
-const Drawer:React.FC<DrawerProps> = props => {
-    const {open=true,close,inline,rootClassname='',rootStyle={},zIndex,title,children,...restProps} = props;
+const Drawer: React.FC<DrawerProps> = props => {
+
+    // 控制遮罩层
+    const [masked, setMasked] = useState(true)
+
+    const { open = true, close, inline, rootClassname = '', rootStyle = {}, zIndex=100, title, children, ...restProps } = props;
     rootStyle.zIndex = zIndex;
-    const classes = classNames('cherry-drawer',rootClassname,{
-        "drawer-inline":inline,
-        
+    const classes = classNames('cherry-drawer', rootClassname, {
+        "drawer-inline": inline,
+
     })
 
-    React.useEffect(()=>{
-        if(open){
-
-        }else{
-
+    React.useEffect(() => {
+        if (open) {
+            if (masked) {
+                setMasked(true)
+            }
+        } else {
+            setMasked(false)
         }
-    },[open])
+    }, [open])
 
-  return (
-    <div className={classes} style={{...rootStyle}} {...restProps}>
-        <div className="cherry-drawer-wrapper">
+    return (
+        <div className={classes} style={{ ...rootStyle }} {...restProps}>
+
+            {/* 遮罩层 */}
+            {/* {masked?<div className='drawer-mask'></div>:null} */}
+            <div className='drawer-mask'></div>
+
             {/* drawer弹出框 */}
-            {open?
-            <div>
-                <header>
-                    {close?<IconClose></IconClose>:null}
-                    <div className='drawer-title'>{title}</div>
-                </header>
-                <main className='alert-body'>{children}</main>
-            </div>:null}
+            <div className="cherry-drawer-wrapper">
+                {open ?
+                    <div className='drawer-content'>
+                        <header className='drawer-header'>
+                            {close ? <IconClose></IconClose> : null}
+                            <div className='drawer-title'>{title}</div>
+                        </header>
+                        <main className='drawer-body'>{children}</main>
+                    </div> : null}
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Drawer
